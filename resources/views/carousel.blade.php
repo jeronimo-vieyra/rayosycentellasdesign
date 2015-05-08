b4d035e93156b8340e21f6356a7b0ce53b4be8c1
jero <juana@Jeronimo-Vieyras-MacBook-Pro.local>
2015-05-08 17:28:45 -0300

    separando

---
+++ resources/views/carousel.blade.php
@@ -0,0 +1,92 @@
+<!-- C A R O U S E L -->
+
+<section>
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="8"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+
+	    <!-- <div class="item active">
+	      <img src="img/carousel/carousel-15.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div> -->
+
+	    <div class="item active">
+	      <img src="img/carousel/carousel-16.jpg" alt="...">
+	      <div class="marco">
+		      <div class="banda_blanca col-xs-12"></div>
+		       <div class="sobre_banda_blanca col-xs-12">
+			      <!-- <img class="center-block" src="img/logo_ryc_negro.png"> -->
+		    	  <div class="carousel-caption">
+		    	  	<h2 class="disciplina">IDENTIDAD CORPORATIVA</h2>
+  		    	  	<!-- <p class="sub-disciplina">hola</p>  -->
+		    	  </div>
+		  	  </div>
+		  </div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-17.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-18.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-19.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-20.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-21.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-22.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-23.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/carousel/carousel-24.jpg" alt="...">
+	      <div class="carousel-caption"></div>
+	    </div>
+
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	    <span class="fa fa-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	    <span class="fa fa-chevron-right" aria-hidden="true"></span> <!-- class="glyphicon glyphicon-chevron-right" -->
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+</section>
