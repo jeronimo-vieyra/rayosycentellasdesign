04ba746a7ba102a00ad559fd19db4de0b8821184
jero <juana@Jeronimo-Vieyras-MacBook-Pro.local>
2015-05-14 19:25:50 -0300

    subi imagenes del portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -25,63 +25,18 @@
                             <div class="row">
                               <div class="portfolio-items isotopeWrapper clearfix" id="3">
 
+
+								<!-- ITEMS webdesign photography print-->
+
                                 <article class="col-md-4 isotopeItem webdesign">
 									<div class="portfolio-item">
-										<img src="img/portfolio/img1.jpg" alt="" />
+										<img src="img/portfolio/aol01.png" alt="" />
 										 <div class="portfolio-desc align-center">
 											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img1.jpg" class="fancybox" rel="gallery1"><i class="fa fa-plus fa-2x" ></i></a>
-												<a href="img/portfolio/img2.jpg" class="fancybox hidden " rel="gallery1"><i class="fa fa-plus fa-2x" ></i></a>
-												<a href="img/portfolio/img2.jpg" class="fancybox hidden " rel="gallery1"><i class="fa fa-plus fa-2x"></i></a>
-											 </div>
-										 </div>
-									</div>
-                                </article>
-
-                                <article class="col-md-4 isotopeItem photography">
-									<div class="portfolio-item">
-										<img src="img/portfolio/img2.jpg" alt="" />
-										 <div class="portfolio-desc align-center">
-											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img2.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
-											 </div>
-										 </div>
-									</div>
-                                </article>
-
-                                <article class="col-md-4 isotopeItem photography">
-									<div class="portfolio-item">
-										<img src="img/portfolio/img3.jpg" alt="" />
-										 <div class="portfolio-desc align-center">
-											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img3.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
-											 </div>
-										 </div>
-									</div>
-                                </article>
-
-                                <article class="col-md-4 isotopeItem print">
-									<div class="portfolio-item">
-										<img src="img/portfolio/img4.jpg" alt="" />
-										 <div class="portfolio-desc align-center">
-											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img4.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
-											 </div>
-										 </div>
-									</div>
-                                </article>
-
-                                <article class="col-md-4 isotopeItem photography">
-									<div class="portfolio-item">
-										<img src="img/portfolio/img5.jpg" alt="" />
-										 <div class="portfolio-desc align-center">
-											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img5.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
+												<h5><a href="#">El arte de vivir</a></h5>
+												<a href="img/portfolio/aol01.png" class="fancybox" rel="gallery1"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/aol02.png" class="fancybox invisible" rel="gallery1"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/aol03.png" class="fancybox invisible" rel="gallery1"><i class="fa fa-plus fa-2x"></i></a>
 											 </div>
 										 </div>
 									</div>
@@ -89,51 +44,157 @@
 
                                 <article class="col-md-4 isotopeItem webdesign">
 									<div class="portfolio-item">
-										<img src="img/portfolio/img6.jpg" alt="" />
+										<img src="img/portfolio/bazze01.png" alt="" />
 										 <div class="portfolio-desc align-center">
 											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img6.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
+												<h5><a href="#">Miguel Bazze</a></h5>
+												<a href="img/portfolio/bazze01.png" class="fancybox" rel="gallery2"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/bazze02.png" class="fancybox invisible" rel="gallery2"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/bazze03.png" class="fancybox invisible" rel="gallery2"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/bazze04.png" class="fancybox invisible" rel="gallery2"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/bazze05.png" class="fancybox invisible" rel="gallery2"><i class="fa fa-plus fa-2x"></i></a>
 											 </div>
 										 </div>
 									</div>
                                 </article>
 
-                                <article class="col-md-4 isotopeItem print">
+								<article class="col-md-4 isotopeItem webdesign">
 									<div class="portfolio-item">
-										<img src="img/portfolio/img7.jpg" alt="" />
+										<img src="img/portfolio/libro_ana01.png" alt="" />
 										 <div class="portfolio-desc align-center">
 											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img7.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
+												<h5><a href="#">Editorial</a></h5>
+												<a href="img/portfolio/libro_ana01.png" class="fancybox" rel="gallery3"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/libro_ana02.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/libro_ana03.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana04.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana05.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana06.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana07.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana08.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana09.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana10.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana11.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana12.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana13.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana14.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana15.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/libro_ana16.png" class="fancybox invisible" rel="gallery3"><i class="fa fa-plus fa-2x"></i></a>
 											 </div>
 										 </div>
 									</div>
                                 </article>
 
-                                <article class="col-md-4 isotopeItem photography">
+                                <article class="col-md-4 isotopeItem webdesign">
 									<div class="portfolio-item">
-										<img src="img/portfolio/img8.jpg" alt="" />
+										<img src="img/portfolio/miel01.png" alt="" />
 										 <div class="portfolio-desc align-center">
 											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img8.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
+												<h5><a href="#">Miel</a></h5>
+												<a href="img/portfolio/miel01.png" class="fancybox" rel="gallery4"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/miel02.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/miel03.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel04.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel05.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel06.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel07.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel08.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel09.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel10.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/miel11.png" class="fancybox invisible" rel="gallery4"><i class="fa fa-plus fa-2x"></i></a>
 											 </div>
 										 </div>
 									</div>
                                 </article>
 
-                                <article class="col-md-4 isotopeItem print">
+                                <article class="col-md-4 isotopeItem webdesign">
 									<div class="portfolio-item">
-										<img src="img/portfolio/img9.jpg" alt="" />
+										<img src="img/portfolio/modelado01.png" alt="" />
 										 <div class="portfolio-desc align-center">
 											<div class="folio-info">
-												<h5><a href="#">Portfolio name</a></h5>
-												<a href="img/portfolio/img9.jpg" class="fancybox"><i class="fa fa-plus fa-2x"></i></a>
-											 </div>
+												<h5><a href="#">Modelado</a></h5>
+												<a href="img/portfolio/modelado01.png" class="fancybox" rel="gallery5"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/modelado02.png" class="fancybox invisible" rel="gallery5"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/modelado03.png" class="fancybox invisible" rel="gallery5"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/modelado04.png" class="fancybox invisible" rel="gallery5"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/modelado05.png" class="fancybox invisible" rel="gallery5"><i class="fa fa-plus fa-2x"></i></a>
+												</div>
 										 </div>
 									</div>
                                 </article>
+
+                                <article class="col-md-4 isotopeItem webdesign">
+									<div class="portfolio-item">
+										<img src="img/portfolio/pilar01.png" alt="" />
+										 <div class="portfolio-desc align-center">
+											<div class="folio-info">
+												<h5><a href="#">Pilar Aguirre Carteras</a></h5>
+												<a href="img/portfolio/pilar01.png" class="fancybox" rel="gallery6"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/pilar02.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/pilar03.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/pilar04.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/pilar05.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/pilar06.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/pilar07.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/pilar08.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/pilar09.png" class="fancybox invisible" rel="gallery6"><i class="fa fa-plus fa-2x"></i></a>
+												</div>
+										 </div>
+									</div>
+                                </article>
+
+                                <article class="col-md-4 isotopeItem webdesign">
+									<div class="portfolio-item">
+										<img src="img/portfolio/rincon01.png" alt="" />
+										 <div class="portfolio-desc align-center">
+											<div class="folio-info">
+												<h5><a href="#">Rincon Natural - Dietética</a></h5>
+												<a href="img/portfolio/rincon01.png" class="fancybox" rel="gallery7"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/rincon02.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/rincon03.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/rincon04.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/rincon05.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/rincon06.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/rincon07.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/rincon08.png" class="fancybox invisible" rel="gallery7"><i class="fa fa-plus fa-2x"></i></a>
+												</div>
+										 </div>
+									</div>
+                                </article>
+
+                                <article class="col-md-4 isotopeItem webdesign">
+									<div class="portfolio-item">
+										<img src="img/portfolio/samsung01.png" alt="" />
+										 <div class="portfolio-desc align-center">
+											<div class="folio-info">
+												<h5><a href="#">Samsung</a></h5>
+												<a href="img/portfolio/samsung01.png" class="fancybox" rel="gallery8"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/samsung02.png" class="fancybox invisible" rel="gallery8"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/samsung03.png" class="fancybox invisible" rel="gallery8"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/samsung04.png" class="fancybox invisible" rel="gallery8"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/samsung05.png" class="fancybox invisible" rel="gallery8"><i class="fa fa-plus fa-2x"></i></a>
+												</div>
+										 </div>
+									</div>
+                                </article>
+
+                                <article class="col-md-4 isotopeItem webdesign">
+									<div class="portfolio-item">
+										<img src="img/portfolio/ypf01.png" alt="" />
+										 <div class="portfolio-desc align-center">
+											<div class="folio-info">
+												<h5><a href="#">YPF</a></h5>
+												<a href="img/portfolio/ypf01.png" class="fancybox" rel="gallery9"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/samsung02.png" class="fancybox invisible" rel="gallery9"><i class="fa fa-plus fa-2x" ></i></a>
+												<a href="img/portfolio/samsung03.png" class="fancybox invisible" rel="gallery9"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/samsung04.png" class="fancybox invisible" rel="gallery9"><i class="fa fa-plus fa-2x"></i></a>
+												<a href="img/portfolio/samsung05.png" class="fancybox invisible" rel="gallery9"><i class="fa fa-plus fa-2x"></i></a>
+												</div>
+										 </div>
+									</div>
+                                </article>
+
+                               
                                 </div>
 
 							</div>
