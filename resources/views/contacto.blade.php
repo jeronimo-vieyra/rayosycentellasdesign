fe6013c969dea3bffc037bd9cfd761b8572fbb39
Guillermina Belli <guille_belli@hotmail.com>
2015-05-14 23:27:37 -0300

    intengro frormulario

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -13,33 +13,79 @@
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2">
 						<div class="cform" id="contact-form">
+
+							{!! Form::open(array('action' => ['ContactosController@enviar_consulta'], 'method' => 'POST' , 'class' => 'form-horizontal contactForm')) !!}
+		
+								<div class="form-group">
+									{!! Form::label('nombre', 'Nombre', array('class' => 'control-label col-md-4')) !!}
+									<div class="col-md-6">
+										{!! Form::text('nombre', null, array('class' => 'form-control')) !!}
+									</div>
+								</div>
+								@if ($errors->first('nombre'))<div class="col-md-6 col-md-offset-4 alert alert-danger">{{$errors->first('nombre')}}</div>@endif
+
+								<div class="form-group">
+									{!! Form::label('email', 'E-mail', array('class' => 'control-label col-md-4')) !!}
+									<div class="col-md-6">
+										{!! Form::text('email',null, array('class' => 'form-control')) !!}
+									</div>
+								</div>
+								@if ($errors->first('email'))<div class="col-md-6 col-md-offset-4 alert alert-danger">{{$errors->first('email')}}</div>@endif
+
+								<div class="form-group">
+									{!! Form::label('asunto', 'Asunto', array('class' => 'control-label col-md-4')) !!}
+									<div class="col-md-6">
+										{!! Form::text('asunto', null, array('class' => 'form-control')) !!}
+									</div>
+								</div>
+								@if ($errors->first('asunto'))<div class="col-md-6 col-md-offset-4 alert alert-danger">{{$errors->first('asunto')}}</div>@endif
+
+								<div class="form-group">
+									{!! Form::label('mensaje', 'Mensaje', array('class' => 'control-label col-md-4')) !!}
+									<div class="col-md-6">
+										{!! Form::textarea('mensaje',null, array('class' => 'form-control', 'rows' => 5)) !!}
+									</div>
+								</div>
+								@if ($errors->first('mensaje'))<div class="col-md-6 col-md-offset-4 alert alert-danger">{{$errors->first('mensaje')}}</div>@endif
+
+								
+								<div class="form-group">
+									<div class="col-md-6 col-md-offset-4">
+										{!! Form::submit('ENVIAR MENSAJE', array('class' => 'btn btn-primary col-md-12')) !!}
+									</div>
+								</div>
+
+							{!! Form::close() !!}
+
+
+<!-- 
 							<div id="sendmessage">
 								 Your message has been sent. Thank you!
 							</div>
 							<form action="contact/contact.php" method="post" role="form" class="contactForm">
 							  <div class="form-group">
-								<label for="name"><?php echo trans('contact.name')?></label>
-								<input type="text" name="name" class="form-control" id="name" placeholder="<?php echo trans('contact.name')?>" data-rule="maxlen:4" data-msg="Please enter at least 4 chars" />
+								<label for="name"><?php //echo trans('contact.name')?></label>
+								<input type="text" name="name" class="form-control" id="name" placeholder="<?php //echo trans('contact.name')?>" data-rule="maxlen:4" data-msg="Please enter at least 4 chars" />
 								<div class="validation"></div>
 							  </div>
 							  <div class="form-group">
-								<label for="email"><?php echo trans('contact.email')?></label>
-								<input type="email" class="form-control" name="email" id="email" placeholder="<?php echo trans('contact.email')?>" data-rule="email" data-msg="Please enter a valid email" />
+								<label for="email"><?php //echo trans('contact.email')?></label>
+								<input type="email" class="form-control" name="email" id="email" placeholder="<?php //echo trans('contact.email')?>" data-rule="email" data-msg="Please enter a valid email" />
 								<div class="validation"></div>
 							  </div>
 							  <div class="form-group">
-								<label for="subject"><?php echo trans('contact.subject')?></label>
-								<input type="text" class="form-control" name="subject" id="subject" placeholder="<?php echo trans('contact.subject')?>" data-rule="maxlen:4" data-msg="Please enter at least 8 chars of subject" />
+								<label for="subject"><?php //echo trans('contact.subject')?></label>
+								<input type="text" class="form-control" name="subject" id="subject" placeholder="<?php //echo trans('contact.subject')?>" data-rule="maxlen:4" data-msg="Please enter at least 8 chars of subject" />
 								<div class="validation"></div>
 							  </div>
 							  <div class="form-group">
-								<label for="message"><?php echo trans('contact.message')?></label>
+								<label for="message"><?php //echo trans('contact.message')?></label>
 								<textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us"></textarea>
 								<div class="validation"></div>
 							  </div>
 
 							  <button type="submit" class="btn btn-theme pull-left">ENVIAR MENSAJE</button>
-							</form>
+							</form> -->
 
 						</div>
 					</div>
